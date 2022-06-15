f2c765721a6c048a8f5cec922089e236ea692a88
Moises4343 <193243@ids.upchiapas.edu.mx>
2022-06-14 20:33:55 -0500

    Change project

---
+++ config/config.js
@@ -0,0 +1,11 @@
+export const api = {
+    port: process.env.PORT || 3000,
+};
+
+export const db = {
+    user: "postgres",
+    host: "localhost",
+    database: "CSDB",
+    password: "193243up",
+    port: "5432",
+};
